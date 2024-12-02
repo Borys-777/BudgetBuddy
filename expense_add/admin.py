e6bb0892a92cdec394bde3671125471d13049769
Borys Shesterniak <1b4r1sdc@students.codeinstitute.net>
2024-12-02 15:26:39 +0000

    Updated admin.py file. Tested adding expenses from admin on the site

--- expense_add/admin.py
+++ expense_add/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Expense
-# from .models import Month
+from .models import Month
 
 # Register your models here.
 admin.site.register(Expense)
+admin.site.register(Month)
