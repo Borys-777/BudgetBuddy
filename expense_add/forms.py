a7f1c09ddeb7e8b58b5b09a7a8142cf4b648cb32
Borys Shesterniak <1b4r1sdc@students.codeinstitute.net>
2024-12-04 14:00:26 +0000

    Created forms.py and added classes for Create and Login for user

---
+++ expense_add/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django import forms
+
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
+
+
+# Create/Register a user here 
+
+class CreateUserForm(UserCreationForm):
+
+    class Meta:
+
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+        # Class for user login 
+
+class LoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
