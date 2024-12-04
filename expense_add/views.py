449744f9379db36af948fe21f49d10ba6d01d0c9
Borys Shesterniak <1b4r1sdc@students.codeinstitute.net>
2024-12-04 17:13:12 +0000

    Updated templates/expense_add folders, fixed the issue with No such template found

--- expense_add/views.py
+++ expense_add/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
  
 class HomePage(TemplateView):
     """
