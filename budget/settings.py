ba613c18c4c13779c7631989de822acfc9adb573
Borys Shesterniak <1b4r1sdc@students.codeinstitute.net>
2024-12-11 18:25:03 +0000

    Fixed the footer on dashboard, user authenticated for logged in users, rep=sponsive table in dashboard

--- budget/settings.py
+++ budget/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-borys777-budgetbuddy-94heqs4qgtb.ws.codeinstitute-ide.net', '.herokuapp.com']
 
